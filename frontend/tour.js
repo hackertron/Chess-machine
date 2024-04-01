f8dbfda3a9bb690f9bdd2d713ae9ba05ae4848f7
hackertron <cooljay.gupta@gmail.com>
2024-04-01 19:06:56 +0530

    Shepherd tour added

--- frontend/tour.js
+++ frontend/tour.js
@@ -20,10 +20,10 @@ const tour = new Shepherd.Tour({
 
   // Add the steps to the tour
   tour.addStep({
-    title: 'Welcome to StumbleUpon',
-    text: 'This is the navbar, which contains links to different parts of the site.',
+    title: 'Welcome to ChessBoard',
+    text: 'Walkthrough this tutorial to understand how to play',
     attachTo: {
-      element: '.navbar',
+      element: '.title',
       on: 'bottom'
     },
     buttons: [
@@ -40,10 +40,10 @@ const tour = new Shepherd.Tour({
   });
 
   tour.addStep({
-    title: 'StumbleUpon Logo',
-    text: 'This is the StumbleUpon logo, which will take you back to the homepage if you click it.',
+    title: 'Main Board',
+    text: 'This is the main game board. When your team reach consensus move will be made automatically on this board. This represent actual game between white and black team',
     attachTo: {
-      element: '.navbar-logo',
+      element: '#board1',
       on: 'bottom'
     },
     buttons: [
@@ -65,10 +65,10 @@ const tour = new Shepherd.Tour({
   });
 
   tour.addStep({
-    title: 'URL Input Field',
-    text: 'This is the URL input field, use it to submit URLs to our site.',
+    title: 'Suggestion Board1',
+    text: 'This is your board where you will play the move that you want to make on main board. If you have white orientation that means you have to make a move first.',
     attachTo: {
-      element: '.url-input',
+      element: '#board2',
       on: 'bottom'
     },
     buttons: [
@@ -90,10 +90,10 @@ const tour = new Shepherd.Tour({
   });
 
   tour.addStep({
-    title: 'Submit Button',
-    text: 'This is the submit button, which you can use to submit the URL you entered in the input field.',
+    title: 'Suggest Move',
+    text: 'Whenever you make a move on suggestion board1, you have to submit it so that your teammate can see your suggested move.',
     attachTo: {
-      element: '#submitUrlButton',
+      element: '#board2-submit',
       on: 'bottom'
     },
     buttons: [
@@ -115,10 +115,10 @@ const tour = new Shepherd.Tour({
   });
 
   tour.addStep({
-    title: 'Upvote Button',
-    text: 'This is the upvote button, which you can use to show your approval of a page.',
+    title: 'Suggestion Board2',
+    text: 'This is the board where you will see your teammate\'s suggested move. If orientation is black then you have to wait for your teammate to make a move first.',
     attachTo: {
-      element: '#upvoteButton',
+      element: '#board3',
       on: 'bottom'
     },
     buttons: [
@@ -140,10 +140,10 @@ const tour = new Shepherd.Tour({
   });
 
   tour.addStep({
-    title: 'Stumble Button',
-    text: 'This is the stumble button, which will take you to a random page on the site.',
+    title: 'Submit counter move',
+    text: 'Submit the counter move against your teammate here.',
     attachTo: {
-      element: '#stumbleButton',
+      element: '#board3-submit',
       on: 'bottom'
     },
     buttons: [
@@ -165,10 +165,10 @@ const tour = new Shepherd.Tour({
   });
 
   tour.addStep({
-    title: 'Downvote Button',
-    text: 'This is the downvote button, which you can use to show your disapproval of a page.',
+    title: 'Consensus Buttons',
+    text: 'Use these buttons to choose which board you agree with. When both teammates agree on same move, move will be made on main board.',
     attachTo: {
-      element: '#downvoteButton',
+      element: '#consensusbox',
       on: 'bottom'
     },
     buttons: [
@@ -190,35 +190,10 @@ const tour = new Shepherd.Tour({
   });
 
   tour.addStep({
-    title: 'OpenChannel button',
-    text: 'To perform actions like upvote and downvote. You must open channel first.',
+    title: 'Game On !!',
+    text: 'Now you are ready to play.',
     attachTo: {
-      element: '#openChannel',
-      on: 'bottom'
-    },
-    buttons: [
-      {
-        text: 'Skip',
-        action: tour.cancel,
-        classes: 'shepherd-button-secondary'
-      },
-        {
-            text: 'Back',
-            action: tour.back,
-            classes: 'shepherd-button-secondary'
-        },
-        {
-            text: 'Next',
-            action: tour.next
-        }
-    ]
-  });
-
-  tour.addStep({
-    title: 'CloseChannel Button',
-    text: 'Once you are done with your upvote/downvote actions. You must use CloseChannel to submit your votes to the chain.',
-    attachTo: {
-      element: '#closeChannel',
+      element: '#consensusbox',
       on: 'bottom'
     },
     buttons: [
@@ -240,8 +215,4 @@ const tour = new Shepherd.Tour({
   });
 
 // Initiate the tour
-if(!localStorage.getItem('shepherd-tour')) {
-  // Start the tour
-  tour.start();
-  localStorage.setItem('shepherd-tour', 'yes');
-}
+tour.start()
